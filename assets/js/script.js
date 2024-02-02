2ca002d15886bc65409b4db592beb808842675b1
Benjamin <benji-fo@outlook.fr>
2024-02-02 17:46:21 +0100

    fix burger menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 var navList = document.querySelector('.nav__list');
+var menu = document.querySelector('.nav__menu');
+var burgermenu = document.querySelector('.burgermenu');
 
 function toggleMenu() {
-    navList.style.display = (navList.style.display === 'none' || navList.style.display === '') ? 'flex' : 'none';
+    navList.classList.toggle('show');
+    navList.style.display = (navList.style.display === "flex") ? "none" : "flex";
+    navList.style.flexDirection = (navList.style.flexDirection === "column") ? "row" : "column";
+    console.log('je clique');
 }
 
-navList.addEventListener('click', toggleMenu);
+menu.addEventListener('click', toggleMenu);
+
+
+// burgermenu.style.display = (burgermenu.style.display === "block") ? "none" : "block";
